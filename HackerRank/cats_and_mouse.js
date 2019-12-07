e2806b1f56e37522766d6a414dc2b7b2c191f0c8
mattblasi <mattblasi@me.com>
2019-12-07 17:39:00 -0500

    Cats and Mouse

---
+++ HackerRank/cats_and_mouse.js
@@ -0,0 +1,62 @@
+/*
+Cats and Mouse
+https://www.hackerrank.com/challenges/cats-and-a-mouse/
+*/
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the catAndMouse function below.
+function catAndMouse(x, y, z) {
+    // check to see what side the cat is on before determining distance
+    let distanceCatA=(z>x) ? z-x : x-z,
+        distanceCatB=(z>y) ? z-y : y-z;
+
+    if (distanceCatA==distanceCatB) return 'Mouse C';
+    return (distanceCatA<distanceCatB) ? 'Cat A' : 'Cat B';
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const xyz = readLine().split(' ');
+
+        const x = parseInt(xyz[0], 10);
+
+        const y = parseInt(xyz[1], 10);
+
+        const z = parseInt(xyz[2], 10);
+
+        let result = catAndMouse(x, y, z);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
